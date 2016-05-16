ecdb26466f3a2fd7b521c4ac7a135f8b0dd114ea
mahmoud-nbet <mahmoud.nb@gmail.com>
2016-05-16 13:12:52 +0100

    update migration script

--- js/script_de_migration.js
+++ js/script_de_migration.js
@@ -1,34 +1,46 @@
-var res = [];
-var s = 0;
-var e = 10;
-var k = 0;
 /*
+var res = []; var e = 10; var s = 0; var k = 0;
 var rr = setInterval(function(){
-
 	if(stores.length >= e){
-
-		tmpStores = stores.slice(s, e);
-		console.log('>>>>', s, e,'length of tmpStores', tmpStores.length);
-
-		s += tmpStores.length;
-		e += tmpStores.length;
-
-
-		for(i in tmpStores){
-			store = tmpStores[i]
-			GMAP.getLatLngFromAddress(store)
-		}
-
+		tmpStores = stores.slice(s, e);console.log('>>>>', s, e,'length of tmpStores', tmpStores.length);
+		s += tmpStores.length;e += tmpStores.length;
+		for(i in tmpStores){store = tmpStores[i]GMAP.getLatLngFromAddress(store)}
 		console.log('i.', k, GMAP.tmp.markers);
 		res = res.concat(GMAP.tmp.markers);
-	}else{
-		clearInterval(rr); console.log('STOP', e, res.length);
-	}
-	k++;
-}, 5000);
+	}else{clearInterval(rr); console.log('STOP', e, res.length);}
+	k++; }, 5000);
 */
 
-// Step 1
+// Step 1 ## load gmap.js in your browser
+// Or copy the flowing object in your console browser
+
+// SMall GMAP Service...
+GMAP = {
+    tmp: { markers: []},
+	getFullAddress: function(store){return store.Adresse+' '+ store.Ville+' '+store.Code_Postal ;},
+    getLatLngFromAddress: function(marker){
+
+        address = this.getFullAddress(marker);
+
+        this.geocoder.geocode({'address': address}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+				// console.log('########## location', results[0].geometry.location);
+                marker.latitude = results[0].geometry.location.lat();
+                marker.longitude = results[0].geometry.location.lng();
+                GMAP.tmp.markers.push(marker);
+            } else {
+				// if address not found
+                marker.latitude = ''; marker.longitude = '';
+                GMAP.tmp.markers.push(marker);
+            }
+		});
+    },
+} 
+
+// Step 2 ## stores is a JSON of all markers
+var stores = []; // JSON array of all markers 
+// Step 3 ## copy and run this flowing code in your console
+var k = 0;
 var rr = setInterval(function(){
 
 	if(k < stores.length){
@@ -39,15 +51,18 @@ var rr = setInterval(function(){
 		console.log('i.', k);
 		//res = res.concat(GMAP.tmp.markers);
 	}else{
-		clearInterval(rr); console.log('STOP', e, res.length);
+		clearInterval(rr); console.log('STOP', e, GMAP.tmp.markers.length + ' markers');
 	}
 	k++;
 }, 2000);
 
-// Step 2
+// Step 4
 stores = GMAP.tmp.markers;
 
-// Step 3
+// Step 5
 storesStr = GMAP.tmp.markers.stringify();
 
-// Step 4 la résultat sur "http://pro.jsonlint.com/" pour récupérer le nouveau JSON avec lat - lng
+// Step 6 ## la résultat sur "http://pro.jsonlint.com/" pour récupérer le nouveau JSON avec lat - lng
+
+// SMall GMAP Service...
+
